cea3e97bbd68da842502c42d9b5484048c66c5c3
sugeng <C:\Documents and Settings\admin\Application Data\The Bat!>
2013-06-01 13:23:42 +0700

    Commit 1

---
+++ src/main/java/com/belajar/domain/Karyawan.java
@@ -0,0 +1,43 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.belajar.domain;
+
+/**
+ *
+ * @author admin
+ */
+
+
+public class Karyawan {
+    private String id;
+    private String nama;
+    private String alamat;
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+    
+}
